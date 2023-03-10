dc32a58b2e5f1fc30c76e6bb5d50474009025246
Andrii55 <117086425+Andrii55@users.noreply.github.com>
2023-03-10 16:25:25 +0200

    Task-1

---
+++ src/js/task-01.js
@@ -0,0 +1,5 @@
+const categoriesList = document.querySelector("#categories");
+const categoriesItem = categoriesList.querySelectorAll("item");
+
+console.log(`Number of categories: ${categoriesItem.length}`);
+
